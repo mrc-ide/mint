f2fd0f1dd45a1bcb6e78e1a97160de3ef83d6841
Alex <alex.hill@gmail.com>
2020-04-21 18:54:04 +0100

    add icon and button

--- src/app/static/src/app/components/app.vue
+++ src/app/static/src/app/components/app.vue
@@ -7,7 +7,7 @@
         </div>
         <div class="navbar navbar-secondary" v-if="currentProject">
             <div class="container-fluid">
-                <div class="navbar-header" style="display: flex; margin-left:-1rem; background:rgba(170, 240, 174, .5)">
+                <div class="navbar-header project-header">
                     <span>{{currentProject.name}}:</span>
                     <drop-down :text="currentRegion.name" parent-class="px-2" toggle-class="text-dark">
                         <div class="dropdown-item" v-for="region in currentProject.regions">
@@ -16,18 +16,15 @@
                     </drop-down>
                 </div>
                 <a href="#" class="px-2 full-height text-dark project-nav">Strategize across regions
-                    <svg height="20" class="octicon octicon-graph" viewBox="0 0 16 16" version="1.1" width="20"
-                         style="margin-left:2px; margin-top: -2px"
-                         aria-hidden="true">
-                        <path fill-rule="evenodd" fill="currentColor"
-                              d="M16 14v1H0V0h1v14h15zM5 13H3V8h2v5zm4 0H7V3h2v10zm4 0h-2V6h2v7z"></path>
-                    </svg>
+                    <b-icon-graph-up></b-icon-graph-up>
                 </a>
             </div>
         </div>
 
         <div class="container pt-5">
-            <a href="#" @click="createProject">Create fake current project to see project navbar</a>
+            <button class="btn btn-primary" @click="createProject">
+                Create fake current project to see project navbar
+            </button>
         </div>
     </div>
 </template>
@@ -35,9 +32,10 @@
     import Vue from "vue"
     import {mapState} from "vuex";
     import dropDown from "./dropDown.vue";
+    import {BIconGraphUp} from "bootstrap-vue";
 
     export default Vue.extend({
-        components: {dropDown},
+        components: {dropDown, BIconGraphUp},
         computed: mapState(["currentProject", "currentRegion"]),
         methods: {
             createProject() {
