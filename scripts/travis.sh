d6ed8a24bbacead29dc2e633e0e1c50b0f7b8e15
Alex <alex.hill@gmail.com>
2020-04-22 12:30:29 +0100

    don't build on pr

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -ex
+HERE=$(dirname $0)
 
 # Do not run if this is a pull request build - don't want to push to master yet
 if [ $TRAVIS_PULL_REQUEST != "false" ]; then
    exit 0
 fi
 
-# TODO: Docker build and push
+$HERE/build-app.sh
+$HERE/push-app.sh
