5f12dce26c6dae3abc945dad1822e2da748b376c
Alex <alex.hill@gmail.com>
2020-04-23 15:39:59 +0100

    test all mutations defined

--- src/app/static/src/tests/store/mutations.test.ts
+++ src/app/static/src/tests/store/mutations.test.ts
@@ -1,7 +1,12 @@
 import {mutations, RootMutation} from "../../app/mutations";
 import {mockRootState} from "../mocks";
+import {expectAllMutationsDefined} from "../testHelpers";
 
 describe("mutations", () => {
+
+    it("implements all defined mutations", () => {
+        expectAllMutationsDefined(RootMutation, mutations);
+    });
 
     it("adds a new project", () => {
         const state = mockRootState()
