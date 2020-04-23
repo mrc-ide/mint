4ca53add7edd13d4948976e0cbdbeca976c3dbff
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-04-23 09:12:53 +0100

    Added graph config endpoint tests

--- src/app/src/test/kotlin/org/imperial/mrc/mint/integration/endpoints/ImpactTests.kt
+++ src/app/src/test/kotlin/org/imperial/mrc/mint/integration/endpoints/ImpactTests.kt
@@ -1,5 +1,12 @@
 package org.imperial.mrc.mint.integration.endpoints
 
-class ImpactTests: EndpointTests() {
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.web.client.getForEntity
 
+class ImpactTests: EndpointTests() {
+    @Test
+    fun `can get impact graph prevalence config`() {
+        val responseEntity = testRestTemplate.getForEntity<String>("/impact/graph/prevalence/config")
+        assertSuccess(responseEntity, "Data")
+    }
 }
