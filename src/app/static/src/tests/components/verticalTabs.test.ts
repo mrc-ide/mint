b8753101131c1ca11d68c4d2a51a6527d259b779
Alex <alex.hill@gmail.com>
2020-04-28 10:14:45 +0100

    add tests

--- src/app/static/src/tests/components/verticalTabs.test.ts
+++ src/app/static/src/tests/components/verticalTabs.test.ts
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import {mount, shallowMount} from "@vue/test-utils";
+import {shallowMount} from "@vue/test-utils";
 import verticalTabs from "../../app/components/verticalTabs.vue";
 
 describe("vertical tabs", () => {
@@ -7,31 +7,34 @@ describe("vertical tabs", () => {
     beforeAll(() => {
         // JSDOM doesn't implement layout so this is always zero by default
         // https://github.com/testing-library/react-testing-library/issues/353
-        Object.defineProperty(HTMLElement.prototype, 'clientWidth', { configurable: true, value: 100 })
+        Object.defineProperty(HTMLElement.prototype, 'clientWidth', {configurable: true, value: 100})
     })
 
     afterAll(() => {
         Object.defineProperty(HTMLElement.prototype, 'clientWidth', 0)
-    })
+    });
+
+    const getWrapper = () => {
+       return shallowMount(verticalTabs, {
+            propsData: {
+                tabs: ["one", "two"],
+                activeTab: "one"
+            }
+        });
+    }
 
     it("displays tabs and active tab", () => {
-        const wrapper = shallowMount(verticalTabs, {propsData: {
-                tabs : [{name: "one", active: false}, {name: "two", active: true}]
-            }});
-
+        const wrapper = getWrapper();
         const tabs = wrapper.findAll("a.nav-link");
         expect(tabs.at(0).text()).toBe("one");
-        expect(tabs.at(0).classes()).not.toContain("active");
+        expect(tabs.at(0).classes()).toContain("active");
 
         expect(tabs.at(1).text()).toBe("two");
-        expect(tabs.at(1).classes()).toContain("active");
+        expect(tabs.at(1).classes()).not.toContain("active");
     });
 
     it("emits tab-selected event when tab is clicked", async () => {
-        const wrapper = shallowMount(verticalTabs, {propsData: {
-                tabs : [{name: "one", active: false}, {name: "two", active: true}]
-            }});
-
+        const wrapper = getWrapper();
         wrapper.findAll("a").at(0).trigger("click");
 
         await Vue.nextTick();
@@ -39,29 +42,25 @@ describe("vertical tabs", () => {
     });
 
     it("calculates tab translation and content margin based on width of element", async () => {
-        const wrapper = mount(verticalTabs, {propsData: {
-                tabs : [{name: "one", active: false}, {name: "two", active: true}]
-            }});
+        const wrapper = getWrapper();
 
         await Vue.nextTick();
         expect(wrapper.find(".nav-tabs").attributes().style)
-            .toBe("transform-origin: 0 0; transform: translate(0, 100px) rotate(-90deg);");
+            .toBe("height: 42px; transform-origin: 0 0; transform: translate(0, 100px) rotate(-90deg);");
         expect(wrapper.find(".col").attributes().style)
             .toBe("margin-left: -58px;");
     });
 
     it("re-calculates tab translation and content margin if tabs change", async () => {
-        const wrapper = mount(verticalTabs, {propsData: {
-                tabs : [{name: "one", active: false}, {name: "two", active: true}]
-            }});
+        const wrapper = getWrapper();
 
         await Vue.nextTick();
         expect(wrapper.find(".nav-tabs").attributes().style)
-            .toBe("transform-origin: 0 0; transform: translate(0, 100px) rotate(-90deg);");
+            .toBe("height: 42px; transform-origin: 0 0; transform: translate(0, 100px) rotate(-90deg);");
         expect(wrapper.find(".col").attributes().style)
             .toBe("margin-left: -58px;");
 
-        Object.defineProperty(HTMLElement.prototype, 'clientWidth', { configurable: true, value: 50 });
+        Object.defineProperty(HTMLElement.prototype, 'clientWidth', {configurable: true, value: 50});
 
         wrapper.setProps({tabs: []});
 
@@ -69,7 +68,7 @@ describe("vertical tabs", () => {
         await Vue.nextTick(); // wait 2 ticks because that's what the watcher does internally
 
         expect(wrapper.find(".nav-tabs").attributes().style)
-            .toBe("transform-origin: 0 0; transform: translate(0, 50px) rotate(-90deg);");
+            .toBe("height: 42px; transform-origin: 0 0; transform: translate(0, 50px) rotate(-90deg);");
         expect(wrapper.find(".col").attributes().style)
             .toBe("margin-left: -8px;");
 
