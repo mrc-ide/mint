50725eef5ab2eb47ab56aaf16f0566c4eea71321
Mark Woodbridge <m.woodbridge@imperial.ac.uk>
2021-06-28 16:15:19 +0100

    Implement UI prerequisites for strategising across regions

--- src/app/static/src/app/index.ts
+++ src/app/static/src/app/index.ts
@@ -3,6 +3,8 @@ import app from "./components/app.vue";
 import {store} from "./store";
 import {router} from "./router";
 
+export const MAX_REGIONS = 15;
+
 export const mint = new Vue({
     el: "#app",
     render: h => h(app),
