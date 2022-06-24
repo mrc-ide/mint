a6150a5fadcc864b3b911b07b1bf65a57c50c4ef
Emma Russell <EmmaRussellKilburn@gmail.com>
2022-06-24 10:52:58 +0100

    update targeted branch and strategise test for new data

--- src/app/static/src/tests/e2e/mint.etest.ts
+++ src/app/static/src/tests/e2e/mint.etest.ts
@@ -78,8 +78,8 @@ test.describe("basic tests", () => {
         await page.click("#stratAcrossRegions");
         await page.click(".btn-primary");
 
-        // Summary table should be displayed, with llin-pbo as at least one of the strategies
-        expect(await page.innerText(".summaryTable")).toContain("Pyrethroid-PBO ITN only");
+        // Summary table should be displayed, with llin as at least one of the strategies
+        expect(await page.innerText(".summaryTable")).toContain("Pyrethroid LLIN only");
 
         // Ensure that heading prompts strategy selection
         expect(await page.innerText("h2 >> nth=1")).toContain("Select a row");
