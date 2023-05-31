7ac961bc6d48abd9a72c4775c912e5195dec9060
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2023-05-31 14:16:43 +0100

    temporarily hide French user guide

--- src/app/static/src/app/featureSwitches.ts
+++ src/app/static/src/app/featureSwitches.ts
@@ -1,5 +1,8 @@
 const urlParams = new URLSearchParams(window.location.search);
 const stratAcrossRegions = !urlParams.get("stratAcrossRegions");
+const frenchUserGuide = urlParams.get("frenchUserGuide");
+
 export const switches = {
-    stratAcrossRegions: stratAcrossRegions
+    stratAcrossRegions,
+    frenchUserGuide
 }
--- src/app/static/src/tests/components/userGuideLinks.test.ts
+++ src/app/static/src/tests/components/userGuideLinks.test.ts
@@ -1,17 +1,23 @@
 import {shallowMount, Wrapper} from "@vue/test-utils";
 import UserGuideLinks from "../../app/components/userGuideLinks.vue";
+import {switches} from "../../app/featureSwitches";
 
 const expectLinksToExpectedResources = (wrapper: Wrapper<any>) => {
     const links = wrapper.findAll("a");
     expect(links.at(0).attributes("href")).toBe("/public/resources/MINT v2.0 User-Guide-English.pdf");
-    expect(links.at(1).attributes("href")).toBe("/public/resources/User-Guide-fr.pdf");
+    if (switches.frenchUserGuide) {
+        expect(links.at(1).attributes("href")).toBe("/public/resources/User-Guide-fr.pdf");
+    } else {
+        expect(links.length).toBe(1);
+    }
 };
 
 describe("User Guide links", () => {
 
     it("renders long version", () => {
         const wrapper = shallowMount(UserGuideLinks);
-        expect(wrapper.findAll("a").at(0).text()).toContain("English");
+        const expectedText = switches.frenchUserGuide ? "English" : "User Guide";
+        expect(wrapper.findAll("a").at(0).text()).toContain(expectedText);
         expectLinksToExpectedResources(wrapper);
     });
 
@@ -21,8 +27,13 @@ describe("User Guide links", () => {
                 short: true
             }
         });
-        expect(wrapper.findAll("a").at(0).text()).toEqual("en");
+        const expectedText = switches.frenchUserGuide ? "en" : "User Guide";
+        expect(wrapper.findAll("a").at(0).text()).toEqual(expectedText);
         expectLinksToExpectedResources(wrapper);
+    });
+
+    it("default frenchUserGuide switch value", () => {
+        expect(!!switches.frenchUserGuide).toBe(false);
     });
 });
 
