fe90b74f4683b37fdaab6ee27acd0f49977e4047
Paul Lietar <pl2113@ic.ac.uk>
2023-12-07 16:36:28 +0000

    Remove unused scripts.
    The `scripts/` folder contained a lot of legacy scripts. The old travis
    CI pipeline has been replaced by equivalent scripts in the `buildkite`
    repo and the deployment tools have been replaced by
    https://github.com/mrc-ide/mint-deploy.
    
    The only files left in that directory relate to local development,
    mostly there to pull and start the mintr docker image.

--- scripts/clear-docker.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-docker rm --force $(docker ps -aq)
-docker network prune --force
-exit 0
--- scripts/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-echo "Clearing docker"
-./scripts/clear-docker.sh
-echo "Starting app"
-./scripts/pull-and-run.sh
-echo "Starting proxy"
-./scripts/proxy.sh
