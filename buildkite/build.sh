ef6f29595a3b7e92d14c5845e46c0efede3f7de1
Paul Lietar <pl2113@ic.ac.uk>
2023-12-07 16:05:21 +0000

    Use multi-staged Docker builds.
    We've been using a nested Docker builds as a way to produce small
    runtime images that don't have to include the whole build environment.
    This is kind of messy and requires a `docker build` and `docker run`
    cycle just to produce the final image.
    
    However Docker provides that feature natively through multi-staged
    builds: https://docs.docker.com/build/building/multi-stage/
    This simplifies the pipeline slightly, and the image may now be built as
    a single step.

--- buildkite/build.sh
+++ buildkite/build.sh
@@ -11,18 +11,11 @@ else
   TAG=$1
 fi
 
-# Create an image based on the shared build env that compiles and builds app image
-docker build --tag=mint-build \
+docker build --tag=$TAG \
   --build-arg GIT_ID=$GIT_ID \
-  --build-arg TAG=$TAG \
   --build-arg SPRING_PROFILE=$1 \
   -f $HERE/build.dockerfile \
   .
-
-# Run image to create app image
-docker run \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  mint-build
 
 # We push created docker image in a separate script so that this script can be re-used
 # to build a local image for front-end integration testing
